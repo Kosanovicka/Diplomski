f754aca677a0632c205bad21ef680fcbfca0e107
Bogdanka Kosanovic <kosanovicbogdanka@gmail.com>
2019-03-11 23:48:10 +0100

    First version.

---
+++ web/javascript/script.js
@@ -0,0 +1,9 @@
+$(function () {
+   $(window).on("scroll", function () {
+     if ($(window).scrollTop() > 50) {
+       $(".navbar").addClass("bg-light");
+     } else {
+       $(".navbar").removeClass("bg-light");
+     }
+   });
+ });
